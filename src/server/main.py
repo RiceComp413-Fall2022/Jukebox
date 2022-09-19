eb6ca8a6e4b3909fe086ef5cc1210ec25989fca0
Austin Hushower <ath8@rice.edu>
2022-09-19 17:19:48 -0500

    Enhanced uri validation and test cases

--- src/server/main.py
+++ src/server/main.py
@@ -1,24 +1,59 @@
 from flask import Flask
 
+from urllib.error import HTTPError
+import re
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-import config
+from src.server import config
 
 app = Flask(__name__)
 
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=config.SPOTIFY_CLIENT_ID,
+                                                           client_secret=config.SPOTIFY_CLIENT_SECRET))
+
 @app.route("/")
 def hello_world():
-    validate_uri("spotify:track:5YZuePCawcrg0DJrWovPu7")
     return "<p>Hello, World!</p>"
 
+
 def validate_uri(uri):
-    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=config.SPOTIFY_CLIENT_ID,
-                                                               client_secret=config.SPOTIFY_CLIENT_SECRET))
+    """
+    The validate uri function returns true or false based on whether or not the provided uri string is a correctly
+    formatted Spotify URI, for an existing song. To return true, the uri must be formatted correctly AND the song must
+    be valid.
+
+    :param uri: string representing the Spotify uri for the request
+    :return: true or false based on whether or not the uri is both correctly formatted AND valid
+    """
+
+    print("Testing uri " + uri)
+
+    # All valid song URIs must contain this string at the start
+    format_indx = uri.find("spotify:track:")
+    if format_indx != 0:
+        return False
+
+    # Verify that remaining characters are valid and of correct length
+    remaining = uri[14:]
+
+    if len(remaining) != 22:
+        return False
+
+    # Assert that the string only contains valid characters
+    if not bool(re.match("^[A-Za-z0-9]*$", remaining)):
+        return False
+
+    # URI is correctly formatted. At this point, the only way to weed out bad URIs is testing + catching exceptions
     try:
         track = sp.track(uri)
-        print(track)
-    except:
-        print("Error fetching test track")
-    else:
-        print("Successfully retrieved test track")
+        return True
+    except HTTPError as err:
+        if err.code == 404:
+            # Track does not exist. Return false.
+            return False
+        else:
+            print("WARNING: Unknown error when querying uri " + uri)
+            return False
+    except spotipy.exceptions.SpotifyException:
+        return False
---
+++ test/server/test_uri_verification.py
@@ -0,0 +1,69 @@
+import unittest
+
+from src.server.main import validate_uri
+
+
+class UriVerificationTest(unittest.TestCase):
+    """
+    Test Class for all URI verification related tests.
+    """
+
+    def test_valid_songs(self):
+        """
+        Tests the uri validator with a list of uris for which the responses should all be true.
+        :return: n/a
+        """
+        print("\nTesting valid song URIs")
+        valid_uris = ["spotify:track:5YZuePCawcrg0DJrWovPu7", "spotify:track:3Ofmpyhv5UAQ70mENzB277", "spotify:track:0qcr5FMsEO85NAQjrlDRKo",
+                      "spotify:track:1IHWl5LamUGEuP4ozKQSXZ"]
+
+        for uri in valid_uris:
+            self.assertTrue(validate_uri(uri), "Valid URI rejected as false. URI: " + uri)
+
+        print("All valid song URIs passed verification.")
+
+    def test_non_song_uris(self):
+        """
+        Tests the uri validator with a list of valid URIs for non-song entities. All should fail.
+        :return: n/a
+        """
+        # Alternative format, valid URIs
+        print("\nTesting correct URIs, for invalid file formats: songs, artists, etc.")
+        non_song_uris = ["spotify:episode:5k48pHc91z9jgzJBAffpxT", "spotify:episode:6q15rdB4spyNp8GukRgrRz",
+                         "spotify:album:3RQQmkQEvNCY4prGKE6oc5",
+                         "spotify:artist:4pQN0GB0fNEEOfQCaWotsY", "spotify:playlist:2pCngqgghHLImIwVBEoyre",
+                         "spotify:user:4dbxzew114d90yduzzpxtzpxv"]
+
+        for uri in non_song_uris:
+            self.assertFalse(validate_uri(uri), "Non-Song, valid uri returned true. URI: " + uri)
+
+        print("All non-song URIs did not pass verification.")
+
+    def test_bad_format_uris(self):
+        """
+        Tests the uri validator with a list of badly formatted uris. All should return false.
+        :return: n/a
+        """
+        # Poorly formatted URIs
+        print("\nTesting poorly formatted URIs.")
+        bad_format_uris = ["", "spootify:track:5YZuePCawcrg0DJrWovPu7",
+                           "spotify:track:5YZuePCawcrspotify:track:5YZuePCawcrg0DJrWovPu7g0DJrWovPu7spotify:track:5YZuePCawcrg0DJrWovPu7"]
+
+        for uri in bad_format_uris:
+            self.assertFalse(validate_uri(uri), "Incorrectly formatted track uri returned true. URI: " + uri)
+
+        print("All bad format URIs did not pass verification.")
+
+    def test_invalid_song_uris(self):
+        """
+        Tests the uri validator with a list of correctly formatted, but invalid song uri ids. All should fail.
+        :return: n/a
+        """
+        # Invalid URIs
+        print("\nTesting correctly formatted, invalid URIs")
+        invalid_uris = ["spotify:track:4YZuePCawcrg0DJrWovPu7", "spotify:track:1IHWl5LamUGEuP4ozKQSXY", "spotify:track:2Ofmpyhv5UAQ70mENzB277"]
+
+        for uri in invalid_uris:
+            self.assertFalse(validate_uri(uri), "Valid format, incorrect URI returned true. URI: " + uri)
+
+        print("All correct format, invalid song URIs failed verification.")
