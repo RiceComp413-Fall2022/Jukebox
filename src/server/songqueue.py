2d8814e6d08d3c2dbd3f5ea60b49c60b7bcf074d
Aedan Cullen <aedan@aedancullen.com>
2022-09-21 14:38:40 -0500

    songqueue: Add docstrings

--- src/server/songqueue.py
+++ src/server/songqueue.py
@@ -1,20 +1,29 @@
+"""Routines related to song queue manangement."""
+
 import heapq
 import threading
 import itertools
 
 class SongQueue:
+    """A song queue which can keep track of song rankings according to upvotes/downvotes."""
 
     def __init__(self):
+        """Create an empty song queue containing no songs. self.counter is monotonic to maintain insertion ordering."""
         self.pq = []
         self.lock = threading.Lock()
         self.counter = itertools.count()
         self.entries_by_song = {}
 
     def __len__(self):
+        """Return the number of songs in the queue."""
         with self.lock:
             return len(self.pq)
 
     def add_song(self, song_data, song_identifier=None):
+        """Add a song data object with zero votes.
+
+        A separate identifier can optionally be provided for use during lookup for voting.
+        """
         song_identifier = song_data if song_identifier is None else song_identifier
         with self.lock:
             count = next(self.counter)
@@ -23,32 +32,39 @@ class SongQueue:
             heapq.heappush(self.pq, entry)
 
     def update_song(self, song_identifier, new_votes):
+        """Completely replace the number of votes that a song has with new_votes."""
         with self.lock:
             entry = self.entries_by_song[song_identifier]
             entry[0] = -new_votes
             heapq.heapify(self.pq)
 
     def upvote_song(self, song_identifier):
+        """Increment the number of votes that a song has. Internally, this means decreasing its priority."""
         with self.lock:
             entry = self.entries_by_song[song_identifier]
             entry[0] -= 1
             heapq.heapify(self.pq)
 
     def downvote_song(self, song_identifier):
+        """Increment the number of votes that a song has. Internally, this means increasing its priority."""
         with self.lock:
             entry = self.entries_by_song[song_identifier]
             entry[0] += 1
             heapq.heapify(self.pq)
 
     def remove_top(self):
+        """Remove the top song in the queue and return a tuple (votes, song_data, song_identifier)."""
         with self.lock:
             while self.pq:
                 priority, count, song_data, song_identifier = heapq.heappop(self.pq)
                 del self.entries_by_song[song_identifier]
-                return song
+                return (-priority, song_data, song_identifier)
             return None
 
     def get_top(self, n):
+        """Return a list of tuples (votes, song_data, song_identifier) for the top n songs in the queue, without removing."""
         with self.lock:
-            # votes, song_data, song_identifier
-            return [(-item[0], item[2], item[3]) for item in heapq.nsmallest(n, self.pq)]
+            ret = []
+            for priority, count, song_data, song_identifier in heapq.nsmallest(n, self.pq):
+                ret.append((-priority, song_data, song_identifier))
+            return ret
