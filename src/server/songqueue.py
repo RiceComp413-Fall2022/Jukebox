84d95e7e1cf1e2e6d0f84f7ace2ae5f81b5fec72
Aedan Cullen <aedan@aedancullen.com>
2022-09-21 14:26:56 -0500

    Add incremental upvote/downvote and optional identifier for lookup

--- src/server/songqueue.py
+++ src/server/songqueue.py
@@ -14,27 +14,41 @@ class SongQueue:
         with self.lock:
             return len(self.pq)
 
-    def add_song(self, song, initial_votes):
+    def add_song(self, song_data, song_identifier=None):
+        song_identifier = song_data if song_identifier is None else song_identifier
         with self.lock:
             count = next(self.counter)
-            entry = [-initial_votes, count, song]
-            self.entries_by_song[song] = entry
+            entry = [0, count, song_data, song_identifier]
+            self.entries_by_song[song_identifier] = entry
             heapq.heappush(self.pq, entry)
 
-    def update_song(self, song, new_votes):
+    def update_song(self, song_identifier, new_votes):
         with self.lock:
-            entry = self.entries_by_song[song]
+            entry = self.entries_by_song[song_identifier]
             entry[0] = -new_votes
             heapq.heapify(self.pq)
 
-    def remove_top(self, song):
+    def upvote_song(self, song_identifier):
+        with self.lock:
+            entry = self.entries_by_song[song_identifier]
+            entry[0] -= 1
+            heapq.heapify(self.pq)
+
+    def downvote_song(self, song_identifier):
+        with self.lock:
+            entry = self.entries_by_song[song_identifier]
+            entry[0] += 1
+            heapq.heapify(self.pq)
+
+    def remove_top(self):
         with self.lock:
             while self.pq:
-                priority, count, song = heapq.heappop(self.pq)
-                del self.entries_by_song[song]
+                priority, count, song_data, song_identifier = heapq.heappop(self.pq)
+                del self.entries_by_song[song_identifier]
                 return song
             return None
 
     def get_top(self, n):
         with self.lock:
-            return [(-item[0], item(2)) for item in heapq.nsmallest(n, self.pq)]
+            # votes, song_data, song_identifier
+            return [(-item[0], item[2], item[3]) for item in heapq.nsmallest(n, self.pq)]
