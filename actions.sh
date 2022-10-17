53a32e0a61e72eb4f51ff4260435707047c101c2
Aedan Cullen <aedan@aedancullen.com>
2022-10-17 15:13:03 -0500

    Use 'python -m pip' in actions

--- actions.sh
+++ actions.sh
@@ -1,8 +1,8 @@
 set -e
 python -m pip install --upgrade pip
-pip install flake8
-pip install flake8-docstrings
-pip install -r src/server/requirements.txt
+python -m pip install flake8
+python -m pip install flake8-docstrings
+python -m pip install -r src/server/requirements.txt
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 flake8 . --count --ignore=E302,E303,E261,D202,D401 --max-complexity=10 --max-line-length=128 --statistics
 JDIR=$(pwd)
