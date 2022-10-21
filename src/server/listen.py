4b97017344207cb2e2016be308cc79befd433db9
Rob <rhg1@rice.edu>
2022-10-21 15:51:32 -0500

    core functionality works need to clean stuff up

--- src/server/listen.py
+++ src/server/listen.py
@@ -6,19 +6,29 @@ from .resources import queue
 from flask import Response
 import json
 
+def stream(testing=False):
+    """ 
+    Defines the stream to be used for server send events
+
+    Set testing to true if you would like the initial q to return and then for the connection to be closed.
+    This option should not be used in prodcution.
+    """
+    messages = announcer.listen()  # returns a queue.Queue
+
+    # tells the user the current state of the queue
+    init_q = format_sse(json.dumps({'uris': [song_id for votes, song, song_id in queue.get_all()]}), SONG_QUEUE_EVENT)
+    yield init_q
+
+    if testing != 0:
+        return
+    
+    while True:
+        msg = messages.get()  # blocks until a new message arrives
+        # check if this message is about the song queue
+        if f"event: {SONG_QUEUE_EVENT}" in msg:
+            yield msg
+
 def song_queue_listen():
     """Listens for a song queue to be announced using server sent events."""
-    def stream():
-        messages = announcer.listen()  # returns a queue.Queue
-
-        # tells the user the current state of the queue
-        init_q = format_sse(json.dumps({'uris': [song_id for votes, song, song_id in queue.get_all()]}), SONG_QUEUE_EVENT)
-        yield init_q
-
-        while True:
-            msg = messages.get()  # blocks until a new message arrives
-            # check if this message is about the song queue
-            if f"event: {SONG_QUEUE_EVENT}" in msg:
-                yield msg
-
+    # NOTE: Change the * to the domain to be more secure later
     return Response(stream(), mimetype='text/event-stream', headers={'Access-Control-Allow-Origin': "*"})
--- test/server/test_song_queue_listen.py
+++ test/server/test_song_queue_listen.py
@@ -3,50 +3,33 @@ Tests that a song queue can be sent end to end.
 
 Doesn't currently work.
 """
-
-import asyncio
+from contextlib import contextmanager
+from functools import partial
+from unittest import mock
 import pytest
-import requests
 
-from multiprocessing import Process
+from src.server.main import app
+from src.server.listen import stream
 
-from sseclient import SSEClient
-from .start_server import start
-from src.server.announcer import SONG_QUEUE_EVENT
+@contextmanager
+def mock_events():
+    with mock.patch('src.server.listen.stream', partial(stream, testing=True)):
+        yield
 
-async def get_message_event(msg_event):
-    """Grabs messages from listen and assigns them to shared variable."""
-    msgs = SSEClient('http://127.0.0.1:5000/songQueueListen')
-    msg_event.set_result(msgs[0].event)
+@pytest.fixture
+def client():
+    app.testing = True
 
-@pytest.mark.asyncio
-@pytest.mark.skip
-async def test_song_queue_listen():
+    with app.test_client() as client:
+        yield client
+
+def test_song_queue_listen(client):
     """
     Subscribe as a song queue listener and see that the server can send song events to us and we can receive it.
-
-    Can't currently get this working but have tested manually
     """
-    # # start server
-    server = Process(target=start)
-    server.start()
+    # start server   
+    with mock_events(): 
+        r = client.get('/songQueueListen')
 
-    loop = asyncio.get_running_loop()
-    msg_event = loop.create_future()
-
-    # listen for song queues
-    listener = loop.create_task(get_message_event(msg_event))
-    # stop flake from yelling at me
-    listener
-
-    await asyncio.sleep(1)
-    requests.get('http://127.0.0.1:5000/addSong')
-
-    await msg_event
-
-    # shutdown server
-    server.terminate()
-    server.join()
-
-    print(msg_event)
-    assert msg_event == SONG_QUEUE_EVENT
+    print(r.data)
+    assert False
