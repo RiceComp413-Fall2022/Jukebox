9adca4582c95ec41b4edd8dec0af2283a1807e05
Thushar Mahesh <thushar.mahesh@gmail.com>
2022-11-08 17:36:13 -0600

    added homepage

--- src/client/src/pages/HomePage.jsx
+++ src/client/src/pages/HomePage.jsx
@@ -1,34 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
-import LoginWrapper from "./LoginWrapper";
 // import { Route } from "react-router";
-import {BrowserRouter as Link} from 'react-router-dom';
+import Spotify from "./Spotify";
+import { reducerCases } from "../utils/Constants";
+import { useStateProvider } from "../utils/StateProvider";
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import PrimaryCreateRoom from "./PrimaryCreateRoom";
 
 export default function HomePage() {
-  const routes  = [
-    {
-        path: "/login",
-        component: LoginWrapper
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+
+  const hash = window.location.hash
+  .substring(1)
+  .split('&')
+  .reduce(function (initial, item) {
+    if (item) {
+      var parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
     }
-  ]
-
-  function handlePrim(){
-    return(
-        <LoginWrapper/>
-    )
-  }
-  function handleCon(){
-
-  }
+    return initial;
+  }, {});
+  window.location.hash = '';
+    let token = hash;
+    if (token.access_token) {
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+    
+    document.title = "Spotify";
+  }, [dispatch,token]);
 
 
   return (
-    <Container>
-        <button onClick={handlePrim}>Primary</button>
+      <div>
+        {token  ? <PrimaryCreateRoom token = {token.access_token}/> :
+        <Container>
+          <Link to='/primary'>
+            <button>
+                Primary
+            </button>
+          </Link>
+          <Link to='/joinCode'>
+            <button>Contributor</button>
+          </Link>
+        </Container>
 
-        <button onClick={handleCon}>Contributor</button>
-    </Container>
-
+        }
+    </div>
   );
 }
 
