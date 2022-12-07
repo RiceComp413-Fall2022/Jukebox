960c8680f5478dc8c01a1503df7146cf3454b8dc
Rob Gonzalez <rhg1@rice.edu>
2022-12-07 15:47:30 -0600

    google sso login works

--- src/client/src/pages/CollabJoinRoom.jsx
+++ src/client/src/pages/CollabJoinRoom.jsx
@@ -1,57 +1,49 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import { reducerCases } from "../utils/Constants";
 import { useStateProvider } from "../utils/StateProvider";
 import { Link } from 'react-router-dom';
-import genUserId from "../components/UserID";
-import { gapi } from "gapi-script";
 import GLogin from "./GLogin";
 
 export default function CollabJoinRoom(props) {
-  const [{ }, dispatch] = useStateProvider();
+  const [{ setUUID }, dispatch] = useStateProvider();
   const inputRef = useRef(null);
-  const userId = genUserId(); // gen userid for the user  
 
   function handleClick() {
     dispatch({
       type: reducerCases.SET_GROUP,
       setGroup: inputRef.current.value,
     });
-
-    // when room is joined also set the userid for this user
-    dispatch({
-      type: reducerCases.SET_UUID,
-      setUUID: userId,
-    });
   }
 
-  // set up google auth
+  // google auth clientId
   const clientId = '865635097502-3r9fj984vh1834te8vq6nur5du57k8cb.apps.googleusercontent.com';
 
-  useEffect(() => {
+  // require Goolge Login until we have a valid userID to use
+  if (setUUID === undefined) {
+    return (
+      <Container>
+        <GLogin clientId={clientId} />
+      </Container>
+    )
+  }
+  else {
+    // there is a valid userID they can now enter the room ID to join
+    console.log('UUID: ' + setUUID);
 
-    const initClient = () => {
-      gapi.client.init({
-        clientId: clientId,
-        scope: ''
-      });
-    };
-
-    gapi.load('client:auth2', initClient);
-  });
-
-  return (
-    <Container>
-      <GLogin clientId={clientId} />
-      <Input ref={inputRef} placeholder="Enter Group ID here" />
-      <Link to={{ pathname: '/queueCollab' }}>
-        <button onClick={handleClick}>
-          Join Room
-        </button>
-      </Link>
-    </Container>
-  );
+    return (
+      <Container>
+        <Input ref={inputRef} placeholder="Enter Group ID here" />
+        <Link to={{ pathname: '/queueCollab' }}>
+          <button onClick={handleClick}>
+            Join Room
+          </button>
+        </Link>
+      </Container>
+    )
+  }
 }
+
 const Input = styled.input.attrs(props => ({
   // we can define static props
   type: "text",
--- src/client/src/pages/GLogin.jsx
+++ src/client/src/pages/GLogin.jsx
@@ -1,15 +1,37 @@
 import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
+import React from "react";
+
+import { reducerCases } from "../utils/Constants";
+import { useStateProvider } from "../utils/StateProvider";
 
 /**
  * Button to login with the google SSO 
  */
 export default function GLogin(props) {
+	const [{}, dispatch] = useStateProvider();
+
+	const initClient = () => {
+		gapi.client.init({
+			clientId: props.clientId,
+			scope: 'https://www.googleapis.com/auth/userinfo.profile'
+		});
+	};
+
+	gapi.load('client:auth2', initClient);
+
 	const onSuccess = (res) => {
-		console.log('success:', res);
+		console.log('successfly used Google SSO, current client GoogleId:', res['googleId']);	
+
+		// set userid to be google user id 
+		dispatch({
+		type: reducerCases.SET_UUID,
+		setUUID: res['googleId'],
+		});
 	};
 
 	const onFailure = (err) => {
-		console.log('failed:', err);
+		console.log('Failed to login with Google SSO:', err);
 	};
 
 	return (
